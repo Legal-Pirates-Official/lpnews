acc3b9ec87c3869256c2081fc6dd68f7b461685f
Lohith <srklohith05@gmail.com>
2022-02-07 16:17:12 +0530

    Changes

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,8 +1,60 @@
-VanillaTilt.init(document.querySelectorAll(".boxes"), {
-    max: 10,
-    speed: 500,
-    scale: 1.05,
-    perspective: 1000,
-    glare: true,
-    'max-glare': 0.3,
+VanillaTilt.init(document.querySelectorAll('.boxes'), {
+	max: 10,
+	speed: 500,
+	scale: 1.05,
+	perspective: 1000,
+	glare: true,
+	'max-glare': 0.3
 });
+
+const newsBoxes = document.querySelector('.news-boxes');
+
+const fetchRequest = (url) => {
+	newsBoxes.innerHTML = ``;
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			data.articles.forEach((article) => {
+				newsBoxes.innerHTML += `
+                    <div class="boxes">
+                        <div class="boxes-img">
+                        ${
+													article.urlToImage
+														? '<img src=' + article.urlToImage + ' alt="" />'
+														: 'No image available'
+												}
+                        </div>
+                        <div class="boxes-content">
+                            <p>${article.title}</p>
+                            <p>${article.content}</p>
+                            <p>
+                                <a href="${
+																	article.url
+																}>">Click here to see more</a>
+                            </p>
+                        </div>
+                    </div>`;
+			});
+		});
+};
+
+addEventListener(
+	'load',
+	fetchRequest(
+		'http://newsapi.org/v2/top-headlines?country=in&apiKey=65d19618ed5f44369343808e565a89cf'
+	)
+);
+
+document.querySelector('.search-input').addEventListener('input', (e) => {
+	fetchRequest(
+		`http://newsapi.org/v2/everything?q=${e.target.value}&apiKey=65d19618ed5f44369343808e565a89cf`
+	);
+});
+
+const categorySelect = (category) => {
+	fetchRequest(
+		'http://newsapi.org/v2/top-headlines?country=in&category=' +
+			category +
+			'&apiKey=65d19618ed5f44369343808e565a89cf'
+	);
+};
